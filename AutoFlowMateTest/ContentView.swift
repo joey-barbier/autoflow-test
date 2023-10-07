520421cb212df773af011b8080554e3329c3a2cb
Joey Barbier <joey.barbier@icloud.com>
2023-10-07 23:01:03 +0200

    Update - content view, message

--- AutoFlowMateTest/ContentView.swift
+++ AutoFlowMateTest/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, Twitch!")
+            Text("Hello, Twitch! @horka_tv")
         }
         .padding()
     }
