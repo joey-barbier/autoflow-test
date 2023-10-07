415e05f7b655a08d393fc02b147abeac7d5aa1ed
Joey Barbier <joey.barbier@icloud.com>
2023-10-08 00:47:53 +0200

    KAN-1 - Update content

--- AutoFlowMateTest/ContentView.swift
+++ AutoFlowMateTest/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, Twitch! @horka_tv")
+            Text("Hello, Twitch! @horka_tv !!")
         }
         .padding()
     }
