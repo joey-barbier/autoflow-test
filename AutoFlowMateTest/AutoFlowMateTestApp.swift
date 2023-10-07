aff7c3a47444000113716b7e8cc6fd03370cf0b4
Joey Barbier <joey.barbier@icloud.com>
2023-10-07 22:23:22 +0200

    First commit

---
+++ AutoFlowMateTest/AutoFlowMateTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoFlowMateTestApp.swift
+//  AutoFlowMateTest
+//
+//  Created by Joey Barbier on 07/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct AutoFlowMateTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
